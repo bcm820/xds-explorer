ec80ab5774b776e13105e0ac55b9948643e11329
bcmendoza <brian.mendoza@deciphernow.com>
2019-05-17 20:44:28 -0400

    basic hello gRPC server test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const { HelloRequest, HelloReply } = require("./protobuf/helloworld_pb.js");
+const { GreeterClient } = require("./protobuf/helloworld_grpc_web_pb.js");
+const client = new GreeterClient("http://localhost:18000");
+const request = new HelloRequest();
+
+/*
+sample DiscoveryRequest
+version_info:
+node: { id: envoy }
+resource_names:
+- foo
+- bar
+type_url: type.googleapis.com/envoy.api.v2.ClusterLoadAssignment
+response_nonce:
+*/
 
 function App() {
+  const [name, setName] = useState("World");
+  const [response, setResponse] = useState("");
+  useEffect(() => {
+    request.setName(name);
+    client.sayHello(request, {}, (err, response) => {
+      if (!err) setResponse(response.getMessage());
+    });
+  }, [name]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          Your Name:{" "}
+          <input type={"text"} onChange={evt => setName(evt.target.value)} />
+        </div>
+        <div>Response: {response}</div>
       </header>
     </div>
   );
